d2216bd740572f8edd716d6d3f100bf853f70864
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2021-08-30 17:01:37 -0300

    Criando projeto back-end

---
+++ aeronave/src/main/java/br/com/teste/sonda/aeronave/AeronaveApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.sonda.aeronave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeronaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeronaveApplication.class, args);
+	}
+
+}
---
+++ aeronave/src/test/java/br/com/teste/sonda/aeronave/AeronaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.sonda.aeronave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeronaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
